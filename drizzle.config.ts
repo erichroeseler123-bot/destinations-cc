4b615573a69db83b7c1be3a07e0b7a33ac642ae9
Erich Roeseler <erichroeseler123@gmail.com>
2026-09-17 07:52:19 -0600

    chore(drizzle): add drizzle.config.ts

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./lib/db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DCC_DATABASE_URL || process.env.DATABASE_URL || "",
+  },
+});
